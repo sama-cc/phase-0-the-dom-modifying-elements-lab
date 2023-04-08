39f9225f983a3b152e1be446665a6442bef51ca8
Sam <sam.camhi@gmail.com>
2023-04-08 18:29:40 -0400

    There is something wrong with this test.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.append(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Sam is the champion";
