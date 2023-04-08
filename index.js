3bfc1f90a1ebf57ebfbb399223f8df4df1fd926f
Sam <sam.camhi@gmail.com>
2023-04-08 18:37:33 -0400

    You have to append last, that is not made clear.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.querySelector('main#main').remove();
 
 const newHeader = document.createElement("h1");
 
-document.body.append(newHeader);
-
 newHeader.id = "victory";
 
 newHeader.textContent = "Sam is the champion";
+
+document.body.append(newHeader);
